fccb07a9ee29714d2b600cfb9345b0798fab6764
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-02-12 00:09:15 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,1 +1,12 @@
-qwertyu
+#!/usr/bin/python3
+"""Define city class"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """
+    Represents a city
+    """
+    state_id = ""
+    name = ""
