e3718fb95647f2793616298d8e82aac0b47a3c5e
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-02-12 00:07:19 +0100

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,1 +1,13 @@
-qwerty
+#!/usr/bin/python3
+"""Defines a review class"""
+
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """
+    Represents a review
+    """
+    place_id = ""
+    user_id = ""
+    text = ""
