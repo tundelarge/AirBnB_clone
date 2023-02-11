8830182e7ce4ac1f11bb8d765bc624d9b08e6227
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-02-11 23:54:58 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,1 +1,12 @@
-qwerty
+#!/usr/bin/python3
+""" Module for User class """
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """
+    Class that represents a user
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
