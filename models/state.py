70aa769861214cc34486a38ad70032cdf57772b9
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-02-12 00:06:22 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,1 +1,10 @@
-qwertyu
+#!/usr/bin/python3
+"""Defines the state class"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    Represent a state
+    """
+    name = ""
