2fb06aa36d882586561141e0cfd6cab365e1401c
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-02-12 23:45:48 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,1 +1,12 @@
-qwertyu
+#!/usr/bin/python3
+"""Defines the Amenity class."""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Represent an amenity.
+    Attributes:
+        name (str): The name of the amenity.
+    """
+
+    name = ""
