013fc644fdfcad926139b1c0eabbc1ab34a91924
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-02-12 23:09:56 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """Define FileStorage class """
-import json
+import json, sys
 from models.base_model import BaseModel
 from models.user import User
 from models.city import City
@@ -11,7 +11,7 @@ from models.amenity import Amenity
 from models.review import Review
 
 class FileStorage:
-    __file__path = 'file.json'
+    __file_path = 'file.json'
     __objects = {}
 
     def all(self):
@@ -28,13 +28,14 @@ class FileStorage:
         objects_dict = {
             obj: objects_copy[obj].to_dict() for obj in objects_copy.keys()
         }
-        with open(FileStorage.__file__path, "w") as f:
+        with open(FileStorage.__file_path, "w") as f:
             json.dump(objects_dict, f)
+
 
     def reload(self):
         """Deserializes the JSON file to __objects"""
         try:
-            with open(FileStorage.__file__path, "r") as f:
+            with open(FileStorage.__file_path, "r") as f:
                 objects_dict = json.load(f)
                 for obj in objects_dict.values():
                     class_name = obj["__class__"]
